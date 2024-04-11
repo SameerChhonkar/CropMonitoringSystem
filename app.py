8548b5d25768adf5d02dca3aef57de2448765c1a
Sameer Chhonkar <92093529+SameerChhonkar@users.noreply.github.com>
2024-04-12 00:13:54 +0530

    init

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# MongoDB connection
+connection_string = "mongodb+srv://Sameer:1234testing@cluster0.tyrcgn5.mongodb.net/"
+mongo_client = MongoClient(connection_string)
+db = mongo_client.CMSDB  # Use your database name
+collection = db.CMSCL  # Use your collection name
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data')
+def get_data():
+    # Fetch latest data from MongoDB
+    latest_data = collection.find_one(sort=[('_id', -1)])
+    if latest_data:
+        return jsonify({
+            'temperature': latest_data['temperature'],
+            'humidity': latest_data['humidity'],
+            'soil_moisture': latest_data['soilmoisture'],
+            'uv_index': latest_data['uvindex']
+        })
+    else:
+        return jsonify({})  # Return empty JSON if no data is found
+
+if __name__ == '__main__':
+    app.run(debug=True)
